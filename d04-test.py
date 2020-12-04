577924d4959595726fa123425c66f8a5be0c69d0
Alex Gulliver <randdalf@gmail.com>
2020-12-04 18:30:17 +0000

    Day 4, Puzzle 2

--- d04-test.py
+++ d04-test.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from d04 import parse, num_valid_passports
+from d04 import parse, num_valid_passports, num_valid_passports_strict
 
 example1 = """ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
@@ -20,10 +20,45 @@ hgt:179cm
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in"""
 
+example2 = """eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007"""
+
+example3 = """pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"""
+
 
 class NumValidPassportsTests(unittest.TestCase):
     def test_example1(slf):
         return slf.assertEqual(num_valid_passports(parse(example1)), 2)
+
+
+class NumValidPassportsStrictTests(unittest.TestCase):
+    def test_example(slf):
+        return slf.assertEqual(num_valid_passports_strict(parse(example2)), 0)
+
+    def test_example3(slf):
+        return slf.assertEqual(num_valid_passports_strict(parse(example3)), 4)
 
 
 if __name__ == "__main__":
--- d04.py
+++ d04.py
@@ -2,17 +2,39 @@
 
 """Advent of Code 2020, Day 4"""
 
+import re
+
 from aoc import solve
 
-required_fields = set([
-    'byr',
-    'iyr',
-    'eyr',
-    'hgt',
-    'hcl',
-    'ecl',
-    'pid'
-])
+height_pattern = re.compile(r'^(\d+)(cm|in)$')
+hair_color_pattern = re.compile(r'^#[0-9a-f]{6}$')
+eye_colors = {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}
+passport_id_pattern = re.compile(r'^[0-9]{9}$')
+
+
+def validate_height(x):
+    match = height_pattern.match(x)
+    if match:
+        height = int(match.group(1))
+        if match.group(2) == 'cm':
+            return 150 <= height <= 193
+        else:
+            return 59 <= height <= 76
+    return False
+
+
+validators = {
+    'byr': lambda x: x.isdigit() and 1920 <= int(x) <= 2002,
+    'iyr': lambda x: x.isdigit() and 2010 <= int(x) <= 2020,
+    'eyr': lambda x: x.isdigit() and 2020 <= int(x) <= 2030,
+    'hgt': validate_height,
+    'hcl': lambda x: hair_color_pattern.match(x) is not None,
+    'ecl': lambda x: x in eye_colors,
+    'pid': lambda x: passport_id_pattern.match(x) is not None,
+    'cid': lambda x: True
+}
+
+required_fields = set(validators.keys()) - {'cid'}
 
 
 def parse(data):
@@ -24,12 +46,26 @@ def parse(data):
         yield passport
 
 
+def has_required_fields(passport):
+    return set(passport.keys()) >= required_fields
+
+
 def num_valid_passports(passports):
     num = 0
     for passport in passports:
-        num += set(passport.keys()) >= required_fields
+        num += has_required_fields(passport)
+    return num
+
+
+def num_valid_passports_strict(passports):
+    num = 0
+    for passport in passports:
+        if has_required_fields(passport):
+            num += sum(
+                validators[field](value) for field, value in passport.items()
+            ) == len(passport)
     return num
 
 
 if __name__ == "__main__":
-    solve(4, parse, num_valid_passports)
+    solve(4, parse, num_valid_passports, num_valid_passports_strict)
