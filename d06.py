44d01aa778e351edd968b0a820660f5034cc551a
Alex Gulliver <randdalf@gmail.com>
2020-12-06 09:29:07 +0000

    Slightly terser day 6

--- d06.py
+++ d06.py
@@ -8,15 +8,15 @@ from aoc import solve
 
 
 def parse(data):
-    return [group.split('\n') for group in data.split('\n\n')]
+    return [[set(p) for p in g.split('\n')] for g in data.split('\n\n')]
 
 
 def anyone_answers(groups):
-    return sum(len(reduce(lambda x, y: x | y, (set(p) for p in g))) for g in groups)
+    return sum(len(reduce(lambda x, y: x | y, g)) for g in groups)
 
 
 def everyone_answers(groups):
-    return sum(len(reduce(lambda x, y: x & y, (set(p) for p in g))) for g in groups)
+    return sum(len(reduce(lambda x, y: x & y, g)) for g in groups)
 
 
 if __name__ == "__main__":
