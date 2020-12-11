f239fbecf0ca31888db11542a1b90da615c075d7
Alex Gulliver <randdalf@gmail.com>
2020-12-11 19:35:14 +0000

    Day 11, Puzzle 2

--- d11-test.py
+++ d11-test.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from d11 import parse, occupied_seats
+from d11 import parse, occupied_seats1, occupied_seats2
 
 example1 = """L.LL.LL.LL
 LLLLLLL.LL
@@ -18,9 +18,14 @@ L.LLLLLL.L
 L.LLLLL.LL"""
 
 
-class OccupiedSeatsTests(unittest.TestCase):
+class OccupiedSeats1Tests(unittest.TestCase):
     def test_example1(slf):
-        return slf.assertEqual(occupied_seats(parse(example1)), 37)
+        return slf.assertEqual(occupied_seats1(parse(example1)), 37)
+
+
+class OccupiedSeats2Tests(unittest.TestCase):
+    def test_example1(slf):
+        return slf.assertEqual(occupied_seats2(parse(example1)), 26)
 
 
 if __name__ == "__main__":
--- d11.py
+++ d11.py
@@ -2,12 +2,25 @@
 
 """Advent of Code 2020, Day 11"""
 
+from itertools import product
+
 from aoc import solve
 from vec2 import Vec2
 
 FLOOR = '.'
 EMPTY_SEAT = 'L'
 OCCUPIED_SEAT = '#'
+
+dirs = [
+    Vec2(-1, -1),
+    Vec2(-1, 0),
+    Vec2(-1, +1),
+    Vec2(0, -1),
+    Vec2(0, +1),
+    Vec2(+1, -1),
+    Vec2(+1, 0),
+    Vec2(+1, +1)
+]
 
 
 class SeatLayout:
@@ -21,35 +34,67 @@ class SeatLayout:
         return slf.seats == otr.seats
 
     def adjacencies(slf, pos):
-        for x in [-1, 0, +1]:
-            for y in [-1, 0, +1]:
-                adj = pos + Vec2(x, y)
-                if adj != pos and adj in slf.seats:
-                    yield slf.seats[adj]
+        return [
+            slf.seats[pos + dir] for dir in dirs if (pos + dir) in slf.seats
+        ]
 
-    def step(slf):
+    def step1(slf):
+        changed = False
         new_seats = {}
         for pos, seat in slf.seats.items():
-            adjacencies = list(slf.adjacencies(pos))
+            adjacencies = slf.adjacencies(pos)
             if seat == EMPTY_SEAT and adjacencies.count(OCCUPIED_SEAT) == 0:
                 seat = OCCUPIED_SEAT
+                changed = True
             elif seat == OCCUPIED_SEAT and adjacencies.count(OCCUPIED_SEAT) >= 4:
                 seat = EMPTY_SEAT
+                changed = True
             new_seats[pos] = seat
-        return SeatLayout(seats=new_seats)
+        slf.seats = new_seats
+        return changed
+
+    def vision(slf, pos):
+        for dir in dirs:
+            test = pos + dir
+            while test in slf.seats and slf.seats[test] == FLOOR:
+                test += dir
+            if test in slf.seats:
+                yield slf.seats[test]
+
+    def step2(slf):
+        changed = False
+        new_seats = {}
+        for pos, seat in slf.seats.items():
+            vision = list(slf.vision(pos))
+            if seat == EMPTY_SEAT and vision.count(OCCUPIED_SEAT) == 0:
+                seat = OCCUPIED_SEAT
+                changed = True
+            elif seat == OCCUPIED_SEAT and vision.count(OCCUPIED_SEAT) >= 5:
+                seat = EMPTY_SEAT
+                changed = True
+            new_seats[pos] = seat
+        slf.seats = new_seats
+        return changed
+
+    def occupied(slf):
+        return list(slf.seats.values()).count(OCCUPIED_SEAT)
 
 
 def parse(data):
     return SeatLayout(data=data)
 
 
-def occupied_seats(layout):
-    while True:
-        new = layout.step()
-        if new == layout:
-            return list(layout.seats.values()).count(OCCUPIED_SEAT)
-        layout = new
+def occupied_seats1(layout):
+    while layout.step1():
+        pass
+    return layout.occupied()
+
+
+def occupied_seats2(layout):
+    while layout.step2():
+        pass
+    return layout.occupied()
 
 
 if __name__ == "__main__":
-    solve(11, parse, occupied_seats)
+    solve(11, parse, occupied_seats1, occupied_seats2)
