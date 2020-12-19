82d44c32a75b42ca0a4ec95a84e8b98c61fca695
Alex Gulliver <randdalf@gmail.com>
2020-12-19 19:07:19 +0000

    Day 19, Puzzle 2

--- d19-test.py
+++ d19-test.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from d19 import parse, matches
+from d19 import parse, part1, part2
 
 example1 = """0: 4 1 5
 1: 2 3 | 3 2
@@ -19,10 +19,66 @@ abbbab
 aaabbb
 aaaabbb"""
 
+example2 = """42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
 
-class SolverTests(unittest.TestCase):
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"""
+
+
+class Part1Tests(unittest.TestCase):
     def test_example1(slf):
-        slf.assertEqual(matches(parse(example1)), 2)
+        slf.assertEqual(part1(parse(example1)), 2)
+
+    def test_example2(slf):
+        slf.assertEqual(part1(parse(example2)), 3)
+
+
+class Part2Tests(unittest.TestCase):
+    def test_example2(slf):
+        slf.assertEqual(part2(parse(example2)), 12)
 
 
 if __name__ == "__main__":
--- d19.py
+++ d19.py
@@ -2,6 +2,7 @@
 
 """Advent of Code 2020, Day 19"""
 
+from itertools import repeat
 import re
 
 from aoc import solve
@@ -21,26 +22,43 @@ def parse(data):
     return Input(data)
 
 
-def build_regex(rules, root='0'):
+def to_regex(rules, root='0', fix=False, n11=1):
     expanded = rules[root]
     while True:
         new = []
         for c in expanded:
             if c in rules:
-                new.append('(')
-                new.extend(rules[c])
-                new.append(')')
+                if fix and c == '8':
+                    new.extend(['(', '42', '+', ')'])
+                elif fix and c == '11':
+                    new.append('(')
+                    new.extend(repeat('42', n11))
+                    new.extend(repeat('31', n11))
+                    new.append(')')
+                else:
+                    new.append('(')
+                    new.extend(rules[c])
+                    new.append(')')
             else:
                 new.append(c)
         if len(new) == len(expanded):
-            return re.compile('^' + ''.join(new) + '$')
+            return  re.compile('^' + ''.join(new) + '$')
         expanded = new
 
 
-def matches(input):
-    pattern = build_regex(input.rules)
+def part1(input):
+    pattern = to_regex(input.rules)
     return sum(pattern.match(msg) is not None for msg in input.messages)
+
+def part2(input):
+    patterns = [to_regex(input.rules, fix=True, n11=i) for i in range(1, 10)]
+    matches = set()
+    for pattern in patterns:
+        for msg in input.messages:
+            if pattern.match(msg):
+                matches.add(msg)
+    return len(matches)
 
 
 if __name__ == "__main__":
-    solve(19, parse, matches)
+    solve(19, parse, part1, part2)
